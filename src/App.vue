9426b7a398ae38c287c7e5aaf72623b61fef96a1
jota <juan.erices04@inacapmail.cl>
2025-11-03 16:37:20 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="bg-red-500 text-white p-4 text-center">
+    Si esto se ve rojo, Tailwind funciona correctamente
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
